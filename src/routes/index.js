4a3db0dad9871d0ad5209ca5d2aefb487226d09c
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-15 12:02:14 -0300

    Páginas e rotas da aplicação

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import React from "react";
 import {Switch} from "react-router-dom";
 import MyRoute from "./MyRoute";
 import Login from "../pages/Login";
+import Aluno from "../pages/Aluno";
+import Alunos from "../pages/Alunos";
+import Fotos from "../pages/Fotos";
+import Register from "../pages/Register";
 import Page404 from "../pages/Page404";
 
 export default function Routes() {
@@ -12,7 +16,17 @@ export default function Routes() {
     // toast.info("Opa, informação!");
     return (
         <Switch>
-            <MyRoute exact path="/" component={Login} />
+            <MyRoute exact path="/" component={Alunos} isClosed={false} />
+            <MyRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
+            <MyRoute exact path="/aluno/" component={Alunos} isClosed />
+            <MyRoute exact path="/fotos/:id" component={Fotos} isClosed />
+            <MyRoute exact path="/login" component={Login} isClosed={false} />
+            <MyRoute
+                exact
+                path="/register"
+                component={Register}
+                isClosed={false}
+            />
             <MyRoute path="*" component={Page404} />
         </Switch>
     );
