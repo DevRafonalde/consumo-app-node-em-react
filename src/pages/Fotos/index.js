4c3ae85eaa83cd229bd8391635232706165208de
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-17 16:53:24 -0300

    Página de edição de fotos
    sem envio para a API

--- src/pages/Fotos/index.js
+++ src/pages/Fotos/index.js
@@ -1,10 +1,52 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+import {get, set} from "lodash";
+import {toast} from "react-toastify";
 import {Container} from "../../styles/GlobalStyles";
+import Loading from "../../components/Loading";
+import {Form, Title} from "./styled";
+import axios from "../../services/axios";
+import history from "../../services/history";
 
 export default function Fotos() {
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [foto, setFoto] = React.useState("");
+    const params = useParams();
+    const id = get(params, "id", "");
+
+    React.useEffect(() => {
+        const getData = async () => {
+            try {
+                setIsLoading(true);
+                const {data} = await axios.get(`/alunos/show/${id}`);
+                setFoto(get(data, "tblFotos[0].url", ""));
+                setIsLoading(false);
+            } catch (error) {
+                setIsLoading(false);
+                toast.error("Erro ao obter imagem");
+                history.push("/");
+            }
+        };
+        getData();
+    }, []);
+
+    const handleChange = (e) => {
+        const fotoEscolhida = e.target.files[0];
+        const fotoEscolhidaUrl = URL.createObjectURL(fotoEscolhida);
+
+        setFoto(fotoEscolhidaUrl);
+    };
+
     return (
         <Container>
-            <h1>Fotos</h1>
+            <Loading isLoading={isLoading} />
+            <Title>Fotos</Title>
+            <Form>
+                <label htmlFor="foto">
+                    {foto ? <img src={foto} alt="foto" /> : "Selecionar"}
+                    <input type="file" id="foto" onChange={handleChange} />
+                </label>
+            </Form>
         </Container>
     );
 }
--- src/pages/Fotos/styled.js
+++ src/pages/Fotos/styled.js
@@ -1,3 +1,26 @@
 import styled from "styled-components";
+import * as colors from "../../config/colors";
 
-export const Paragrafo = styled.p``;
+export const Form = styled.form`
+    label,
+    img {
+        width: 180px;
+        height: 180px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: #eee;
+        border: 5px dashed ${colors.primaryColor};
+        border-radius: 50%;
+        margin: 30px auto;
+        cursor: pointer;
+    }
+
+    input {
+        display: none;
+    }
+`;
+
+export const Title = styled.h1`
+    text-align: center;
+`;
