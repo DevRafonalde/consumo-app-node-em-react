c5726c3b781a60f20780cb085206fdbcda8a1253
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-17 09:08:13 -0300

    Correção roteamento

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,7 +18,7 @@ export default function Routes() {
         <Switch>
             <MyRoute exact path="/" component={Alunos} isClosed={false} />
             <MyRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
-            <MyRoute exact path="/aluno/" component={Alunos} isClosed />
+            <MyRoute exact path="/aluno/" component={Aluno} isClosed />
             <MyRoute exact path="/fotos/:id" component={Fotos} isClosed />
             <MyRoute exact path="/login" component={Login} isClosed={false} />
             <MyRoute
