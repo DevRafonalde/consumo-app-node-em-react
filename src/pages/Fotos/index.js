d395b16f0f5ba9712277fa7a0d499193ac6e511d
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-18 07:46:29 -0300

    Envio de fotos dos alunos pro servidor

--- src/pages/Fotos/index.js
+++ src/pages/Fotos/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {useParams} from "react-router-dom";
-import {get, set} from "lodash";
+import {useDispatch} from "react-redux";
+import {get} from "lodash";
 import {toast} from "react-toastify";
 import {Container} from "../../styles/GlobalStyles";
 import Loading from "../../components/Loading";
 import {Form, Title} from "./styled";
 import axios from "../../services/axios";
 import history from "../../services/history";
+import * as actions from "../../store/modules/auth/actions";
 
 export default function Fotos() {
+    const dispatch = useDispatch();
     const [isLoading, setIsLoading] = React.useState(false);
     const [foto, setFoto] = React.useState("");
     const params = useParams();
@@ -30,11 +33,33 @@ export default function Fotos() {
         getData();
     }, []);
 
-    const handleChange = (e) => {
+    const handleChange = async (e) => {
         const fotoEscolhida = e.target.files[0];
         const fotoEscolhidaUrl = URL.createObjectURL(fotoEscolhida);
 
         setFoto(fotoEscolhidaUrl);
+
+        const formData = new FormData();
+        formData.append("aluno_id", id);
+        formData.append("arquivo", fotoEscolhida);
+
+        try {
+            setIsLoading(true);
+            await axios.post("/fotos/", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+            setIsLoading(false);
+            toast.success("Foto enviada com sucesso");
+        } catch (error) {
+            const {status} = get(error, "response", "");
+            toast.error("Erro ao enviar foto");
+            if (status === 401) {
+                dispatch(actions.loginFailure());
+            }
+            setIsLoading(false);
+        }
     };
 
     return (
